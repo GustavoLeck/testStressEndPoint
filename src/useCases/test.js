193eca3a7b10b3744f40099a68b825526c34ef42
Gustavo Leck <gustavoleck@hotmail.com>
2023-10-23 01:12:41 -0300

    fix(ajustado criacao de logs de erro)

--- src/useCases/test.js
+++ src/useCases/test.js
@@ -2,17 +2,15 @@ import { TestGetApi } from "../api/test-get-api.js";
 import { TestPostApi } from "../api/test-post-api.js";
 import { TestPutApi } from "../api/test-put-api.js";
 import { TestDeleteApi } from "../api/test-delete-api.js";
-import { CreateUserDatabase } from "../database/create-log-database.js";
-
+import { CreateLogDatabase } from "../database/create-log-database.js";
 
 export class Test {
     async execute(value) {
-
         const { type, url, quantidade, tempo, idTeste, body } = value;
 
-        for (let index = 1; index < quantidade; index++) {
+        for (let index = 1; index <= quantidade; index++) {
             await new Promise((resolve) => setTimeout(resolve, tempo));
-            console.log("-------------------------------------------------");
+            console.log("---------------------------");
             console.log("Operação: " + index);
             try {
                 let response = {};
@@ -29,18 +27,16 @@ export class Test {
                     response = await new TestDeleteApi().execute(url, body, idTeste);
                 }
                 if (!response.status) {
-                    throw new Error(response)
+                    const createLog = await new CreateLogDatabase().execute(response);
+                    if (createLog.status) {
+                        console.log("   =>Log criado com sucesso");
+                    } else {
+                        console.log("   =>Erro ao criar log");
+                    }
                 }
                 console.log("Teste Realizado com sucesso")
             } catch (erro) {
-                console.log(erro)
-                const erroFormatdo = JSON.stringify(erro);
-                const createLog = await new CreateUserDatabase().execute(erroFormatdo)
-                if (createLog.status) {
-                    console.log("   =>Log de erro criado com sucesso");
-                } else {
-                    console.log("   =>Erro ao criar log");
-                }
+                console.log(erro);
             }
         }
         return;
